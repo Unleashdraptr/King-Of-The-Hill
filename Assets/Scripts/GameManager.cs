bd21520b0edec846162cd6dcbcf73e703978d222
Unleashdraptr <98326571+Unleashdraptr@users.noreply.github.com>
2023-10-17 13:16:24 +0100

    Simple Setup
    Have all the basics like the Arena Layout and the base Concept of the UI. Will now branch off to work on my AI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // This is where we'll have the game states be sorted e.g., Win, Lose, Pausing
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
